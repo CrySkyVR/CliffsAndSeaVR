efffd67617091ac2728234edff8b0aba7b5eec29
sirelion <sirelion@gmail.com>
2016-11-11 02:20:11 +0100

    Add faro & monster

---
+++ Assets/SCRIPTS/Camera_initial_angle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_initial_angle : MonoBehaviour {
+
+
+	//public Transform angulo;
+	public Vector3 direccionInicial;
+
+	// Use this for initialization
+	void Start () {
+		//angulo=GetComponent<Transform>()=new V
+		transform.LookAt (direccionInicial);
+	
+	}
+	
+
+}
---
+++ Assets/SCRIPTS/Facing.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Facing : MonoBehaviour {
+
+	public Transform objetivo;
+	public Vector3 direccionInicial;
+
+
+
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.LookAt (objetivo,direccionInicial);
+	
+	}
+}
